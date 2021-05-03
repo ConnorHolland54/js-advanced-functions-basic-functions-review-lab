7169bd4f588a4cdfbfab975028ac39f8036fd5f0
Connor Holland <supersoccergoalie123@gmail.com>
2021-05-03 17:25:50 -0600

    commit tree 6bf38e17e017bd52185ab64dddd8d90c62677bc1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(work = "go to the office") {
+  return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(style="*") {
+  return function(adjective="special") {
+    return `You are ${style}${adjective}${style}!`
+  }
+}
+
+let Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b;
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+function actionApplyer(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++) {
+    a = ray[i](a)
+  }
+  return a
+}
